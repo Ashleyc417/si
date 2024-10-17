80f98fcd87d0ea61107af09b09d97fc34b06ad00
Ashleyc417 <ashleyc417@gmail.com>
2024-10-17 15:09:05 -0700

    Update display comment

--- cpsc121/composition/ShoppingCart.h
+++ cpsc121/composition/ShoppingCart.h
@@ -20,10 +20,17 @@ class ShoppingCart {
     // displayShoppingCart will print the contents of a single instance of
     // a Shopping cart including the customer and their items.
     // Ex.
+    // ----------------------------------------------
     // Ashley's Shopping Cart:
-    // Item 1: Bouncy Ball              Price: $1.00
-    // Item 2: Flower Lego Set          Price: $50.00
-    // Item 3: Yogurt                   Price: $1.50
+    // ----------------------------------------------
+    // Item 1: Bouncy Ball              
+    // Price: $1.00
+    //
+    // Item 2: Flower Lego Set          
+    // Price: $50.00
+    //
+    // Item 3: Yogurt                   
+    // Price: $1.50
     void displayShoppingCart();
 
   private:
