d4cb839db1d646e6f80eccb5f5530ff5fa667100
ashley <ashleyc417@csu.fullerton.edu>
2024-10-23 22:39:20 -0700

    add pointer exercise

--- cpsc121/pointers/main_solved.cpp
+++ cpsc121/pointers/main_solved.cpp
@@ -3,6 +3,11 @@
 // TODO: Part 2 - Write a function that swaps two values using pointers.
 void swapValues(int *ptr1, int *ptr2) {
   // TODO: Implement swap logic using pointers.
+ 
+  int tmp = *ptr1;
+
+  *ptr1 = *ptr2;
+  *ptr2 = tmp;
 }
 
 int main() {
@@ -11,7 +16,8 @@ int main() {
   int a = 10;
   int *p = &a;
 
-  // TODO: Part 1 - Use the pointer to change the value of 'a' to 20.
+  // TODO: Part 1 - Use the pointer `p` to change the value of 'a' to 20.
+  *p = 20;
 
   if (a == 20) {
     std::cout << "Test 1 passed!" << std::endl;
@@ -23,6 +29,7 @@ int main() {
   int x = 15, y = 25;
 
   // TODO: Call swapValues() and pass the addresses of x and y
+  swapValues(&x, &y);
 
   if (x == 25 && y == 15) {
     std::cout << "Test 2 passed!" << std::endl;
@@ -33,7 +40,8 @@ int main() {
   // Test for Part 3
   int **pp = &p;
 
-  // TODO: Part 3 - Use pp to change the value of 'a' to 30.
+  // TODO: Part 3 - Use `pp` to change the value of 'a' to 30.
+  **pp = 30;
 
   if (a == 30) {
     std::cout << "Test 3 passed!" << std::endl;
