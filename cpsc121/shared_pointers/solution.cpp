ec361117f1811a5d573d82c1dfce094a67d96f3c
Ashley Chan <111904296+Ashleyc417@users.noreply.github.com>
2024-11-05 15:48:15 -0800

    Update solution.cpp

--- cpsc121/shared_pointers/solution.cpp
+++ cpsc121/shared_pointers/solution.cpp
@@ -21,17 +21,20 @@ private:
 };
 
 int main() {
-    // TODO: Step 1 - Create a shared pointer `p1` to a new Task with the name "Finish assignment"
+    // TODO: Step 1 - Create a shared pointer `p1` to a new `Task` with the name "Finish assignment"
+    // Then, print out p1's use count (refer to the README.md)
     // This creates a shared_ptr to a Task and displays the initial reference count.
     std::shared_ptr<Task> p1 = std::make_shared<Task>("Finish assignment");
     std::cout << "Reference count after creating p1: " << p1.use_count() << std::endl;
 
     // TODO: Step 2 - Create a second shared pointer `p2` that shares ownership with `p1`
+    // Then, print out p2's use count (refer to the README.md)
     // Assign `p1` to `p2`, which increases the reference count.
     std::shared_ptr<Task> p2 = p1;
     std::cout << "Reference count after creating p2: " << p1.use_count() << std::endl;
 
     // TODO: Step 3 - Create a third shared pointer `p3` that also shares ownership with `p1`
+    // Then, print out p3's use count (refer to the README.md)
     // Assign `p1` to `p3`, further increasing the reference count.
     std::shared_ptr<Task> p3 = p1;
     std::cout << "Reference count after creating p3: " << p1.use_count() << std::endl;
