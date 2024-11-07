096e935ac1fdd44fcd49d39a26693ddeaab444bb
Ashleyc417 <ashleyc417@gmail.com>
2024-11-07 09:29:34 -0800

    Add example from model 2 on the worksheet

---
+++ cpsc121/iterators_model2/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <ostream>
+#include <string>
+#include <map>
+
+int main() {
+  std::map<std::string, int> item_to_qty {
+    {"milk", 4},
+    {"banana", 5},
+    {"water", 10}
+  };
+  // TODO: Create an iterator for item_to_qty called `it`    
+  // pointing to the first element of the container. 
+  std::map<std::string, int>::iterator it = item_to_qty.begin();
+
+  // TODO: Check if iterator object `it` is not yet pointing to the
+  // element following the last element of the map
+  while(it != item_to_qty.end()) {
+
+    // TODO: Display the key of the current map element
+    std::cout << "key: " << it->first << std::endl;
+
+    // TODO: Display the value of the current map element
+    std::cout << "value: " << it->second << std::endl;
+
+    std::cout << "\n";
+    // TODO: Move `it` to the next map element
+    it++;
+
+  }
+  return 0;
+}
