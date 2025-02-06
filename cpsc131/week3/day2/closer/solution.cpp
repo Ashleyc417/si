4f4d04a41dadd576b0375e30c06e15a1fdff4d2b
Ashleyc417 <ashleyc417@gmail.com>
2025-02-05 17:11:24 -0800

    move closer to day2

---
+++ cpsc131/week3/day2/closer/solution.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+// Prompt: Write a program to determine how far a number is from 10.
+// Details: Accept the number as an integer input from the user and print the answer to 
+//          stdout (std::cout).
+
+int main() {
+    int num;
+    std::cout << "Enter a number: ";
+    std::cin >> num;
+
+    int distance = std::abs(num - 10);  // Absolute difference
+    std::cout << "Distance from 10: " << distance << std::endl;
+
+    return 0;
+}
